f7a93e1fe7940a89a9503c73f441120c855fb950
zhangzhiwei <zhangzhiwei_94@163.com>
2016-12-02 18:41:37 +0800

    新增每30s刷新页面

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
-	echo "heool";
-?>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="author" content="Zhangzhiwei">
+	<meta http-equiv="refresh" content="10">
+	<title>管理员界面</title>
+
+</head>
+<body>
+	<?php
+// 		require 'conn.php';
+		
+	?>
+</body>
+
+
+</html>
