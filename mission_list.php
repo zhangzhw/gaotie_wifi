2556f9f249295def27d78f1eba21031ed03567f7
zhangzhiwei <zhangzhiwei_94@163.com>
2016-12-02 21:38:08 +0800

    增加了滚动条

--- mission_list.php
+++ mission_list.php
@@ -7,6 +7,7 @@
 </head>
 <body>
 <div>
+	<h2>问卷任务</h2>
 	<ol>
 		<li><a href="play.php">公益广告</a></li>
 	
@@ -14,11 +15,20 @@
 	</ol>
 </div>
 <div>
+	<h2>广告任务</h2>
+	<div style="width:400px;height:100px;overflow:auto">
 	<ol>
-		<li><a href="play.php">公益广告</a></li>
-	
+		<li><a href="play.php">公益广告1</a></li>
+		<li><a href="play.php">公益广告2</a></li>
+		<li><a href="play.php">公益广告3</a></li>
+		<li><a href="play.php">公益广告4</a></li>
+		<li><a href="play.php">公益广告5</a></li>
+		<li><a href="play.php">公益广告6</a></li>
+		<li><a href="play.php">公益广告7</a></li>
+		<li><a href="play.php">公益广告8</a></li>
 	
 	</ol>
+	</div>
 
 
 </div>
