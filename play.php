ad30250d0b3ea8109824ca5ab92f873d7d72b879
zhangzhiwei <zhangzhiwei_94@163.com>
2016-12-02 20:00:05 +0800

    更改了格式

--- play.php
+++ play.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="author" content="Zhangzhiwei">
+	<title>媒体播放</title>
+</head>
 <body>
-<video src="laji.mp4" controls="controls" >
-你的浏览器不支持
-</video>
+<div style="text-align:center">
+	<h1>公益广告</h1>
+	<video src="laji.mp4" controls="controls" >
+	你的浏览器不支持
+	</video>
+</div>
+	
 
 
 </body>
