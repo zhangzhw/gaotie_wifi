d4895e36a714cc8f8c7304d0120775aac1686930
zhangzhiwei <zhangzhiwei_94@163.com>
2016-12-03 20:17:37 +0800

    mysql更新成mysqli

--- conn.php
+++ conn.php
@@ -1,43 +1,24 @@
-
-<html>
-<head>
-<title>������м�¼</title>
-</head>
-<body>
-
-
 <?php
-//���ݿ������ļ�
 
 //error_reporting(0);
-$host='127.0.0.1';//���ݿ������
-$user='root';//���ݿ��û���
-$password='';//���ݿ�����
-$database='gaotie';//���ݿ���
-$conn=@mysql_connect($host,$user,$password) or die('���ݿ�����ʧ�ܣ�');
-@mysql_select_db($database) or die('û���ҵ����ݿ⣡');
-mysql_query("set names 'gb2312'");
-
-
-
-$sql="select * from deceive";
-$result=mysql_query($sql);
-
-echo "<table border=1>";     //ʹ�ñ���ʽ������
-echo "<tr><td>ID</td><td>剩余流量</td><td>已用流量</td></tr>";
-
-while($row=mysql_fetch_array($result))
+$host='127.0.0.1';
+$user='root';
+$password='';
+$database='gaotie';
+$con=mysqli_connect($host,$user,$password,$database);
+if(mysqli_connect_errno($con))
 {
-	echo "<tr>";
-	echo "<td>".$row['deceive_id']."</td>";   //��ʾID
-	echo "<td>".$row['left_bandwidth']." </td>";  //��ʾ����
-	echo "<td>".$row['totaluse']." </td>";   //��ʾ����
-	echo "</tr>";
+	echo "连接MySQL失败：".mysqli_connect_error();
 }
+$sql="select * from device";
+$result=mysqli_query($con, $sql);
 
-echo "</table>";
+$device=mysqli_fetch_all($result,MYSQLI_ASSOC);
+
+echo "device_id:".$device[0]["device_id"]." left_bandwidth".$device[0]["left_bandwidth"];
+
+
+
+
 
 ?>
-
-</body>
-</html>
