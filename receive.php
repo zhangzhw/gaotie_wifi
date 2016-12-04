7f4d2d016d92178020dd642373fbc5d18c3568ae
zhangzhiwei <zhangzhiwei_94@163.com>
2016-12-04 15:53:39 +0800

    和app交互的

---
+++ receive.php
@@ -0,0 +1,39 @@
+<?php 
+require 'conn.php';
+if ($_SERVER['REQUEST_METHOD']=='POST')
+{
+	$post_data = file_get_contents('php://input');
+	$data = json_decode($post_data, true);
+
+	if($data["type"]==1)
+	{
+		$device_id=$data["device_id"];
+		$past_use=$data["past_use"];
+		$sql="select * from device where device_id='".$device_id."'";
+		$result=exec_select_sql($sql);
+		$ret=array("left_bandwidth"=>"2048","permission"=>"0");
+		if(count($result)>0)
+		{
+			$ret["left_bandwidth"]=$result[0]["left_bandwidth"]-$past_use;
+			$ret["permission"]=$result[0]["permission"];
+			$toaluse=$result[0]["totaluse"]+$past_use;
+			$sql="update device set left_bandwidth='".$ret["left_bandwidth"]."',totaluse='".$toaluse."' where device_id='".$device_id."'";
+			exec_upt_sql($sql);
+			echo json_encode($ret);
+		}
+		else
+		{
+			$sql="insert into device values('".$device_id."',".$ret["left_bandwidth"].",0,0)";
+			exec_upt_sql($sql);
+			echo json_encode($ret);
+		}
+	}
+	
+	
+	
+	
+//  	echo $data["type"];
+}
+
+
+?>
--- spec_test.php
+++ spec_test.php
@@ -1,14 +1,28 @@
 <?php 
 require 'conn.php';
+// $data=array("device_id"=>"dec110","past_use"=>"10");
 
-// if (exec_upt_sql("insert into device(device_id,left_bandwidth,totaluse) values ('ssa',50,0)"))
-// 	echo "\nchenggong";
-// else
-// 	echo "\nshibai";
-echo "1";
 
-$device=exec_select_sql("select * from device where device_id='dec110'");
-// echo count($device);
-echo "device_id:".$device[0]["device_id"]." left_bandwidth".$device[0]["left_bandwidth"];
+// $device_id=$data["device_id"];
+// $past_use=$data["past_use"];
+// $sql="select * from device where device_id='".$device_id."'";
+// $result=exec_select_sql($sql);
+
+// $ret=array("left_bandwidth"=>"2048","permission"=>"0");
+// if(count($result)>0)
+// {
+	
+// 	$ret["left_bandwidth"]=$result[0]["left_bandwidth"]-$past_use;
+// 	$ret["permission"]=$result[0]["permission"];
+// 	$toaluse=$result[0]["totaluse"]+$past_use;
+// 	$sql="update device set left_bandwidth='".$ret["left_bandwidth"]."',totaluse='".$toaluse."' where device_id='".$device_id."'";
+// 	exec_upt_sql($sql);
+// }
+
+$device_id="fds";
+$sql="insert into device values('".$device_id."',,,)";
+echo $sql;
+exec_upt_sql($sql);
+
 
 ?>
