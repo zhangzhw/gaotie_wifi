16cf5dca53924f8351f8b9b1d7f4df6c26fdf77b
zhangzhw <zhangzhiwei_94@163.com>
2016-12-08 21:15:38 +0800

    app对接细化

--- receive.php
+++ receive.php
@@ -5,7 +5,7 @@ if ($_SERVER['REQUEST_METHOD']=='POST')
 	$post_data = file_get_contents('php://input');
 	$data = json_decode($post_data, true);
 
-	if($data["type"]==1)//首次接入的时候给予流量
+	if($data["type"]==1)//首次接入的时候给予流量，及以后每次查询流量和权限
 	{
 		$device_id=$data["device_id"];
 		$past_use=$data["past_use"];
@@ -35,7 +35,19 @@ if ($_SERVER['REQUEST_METHOD']=='POST')
 		$result=exec_select_sql($sql);
 		echo json_encode($result);
 	}
-	
+	elseif($data["type"]==3)//广告地址
+	{
+		$task_id=$data["task_id"];
+		$sql="select url from ad_table where task_id=".$task_id;
+		$result=exec_select_sql($sql);
+		echo json_encode($result);
+	}
+	elseif($data["type"]==4)//获取资源列表及其地址
+	{
+		$sql="select * from resoure_tb";
+		$result=exec_select_sql($sql);
+		echo json_encode($result);
+	}
 	
 }
 
